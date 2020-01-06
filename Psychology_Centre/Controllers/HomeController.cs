77a4ba03a37f710ffcedb38672ba41ea049dec22
Valani <cas23473@gmail.com>
2020-01-06 20:34:34 +0200

    first change

--- Psychology_Centre/Controllers/HomeController.cs
+++ Psychology_Centre/Controllers/HomeController.cs
@@ -11,27 +11,10 @@ namespace Psychology_Centre.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public ViewResult Index()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            int hour = DataTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "good afternoon";
         }
     }
 }
