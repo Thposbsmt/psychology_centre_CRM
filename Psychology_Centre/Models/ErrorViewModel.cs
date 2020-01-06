4e21087ce7d32c9f82866b8242a11e4ce6d1c210
Thposbsmt <thposbsmt@protonmail.com>
2020-01-06 22:07:34 +0500

    Initialization

---
+++ Psychology_Centre/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Psychology_Centre.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
